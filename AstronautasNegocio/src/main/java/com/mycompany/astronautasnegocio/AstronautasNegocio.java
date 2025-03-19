62c4ecdfe2a9d1ab609a882d4d4becda3da14156
Oribiel <153157241+Oribiel@users.noreply.github.com>
2025-03-18 19:32:58 -0600

    C
    c

---
+++ AstronautasNegocio/src/main/java/com/mycompany/astronautasnegocio/AstronautasNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.astronautasnegocio;
+
+/**
+ *
+ * @author oribi
+ */
+public class AstronautasNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
