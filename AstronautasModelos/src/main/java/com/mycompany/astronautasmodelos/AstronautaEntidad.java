be019f288646eaee47d16dd7cc5712e4084ad966
Oribiel <153157241+Oribiel@users.noreply.github.com>
2025-03-18 20:05:22 -0600

    arquitectura astronauta
    crear

---
+++ AstronautasModelos/src/main/java/com/mycompany/astronautasmodelos/AstronautaEntidad.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.astronautasmodelos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author oribi
+ */
+
+public class AstronautaEntidad implements Serializable {
+
+  
+    
+}
